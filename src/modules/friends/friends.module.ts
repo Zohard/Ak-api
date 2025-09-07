2f705d65793ad67bb3bc2eea9233c9fbc8a0a2fd
Zohardus <zohard@hotmail.fr>
2025-09-07 04:15:21 +0200

    build fix dependency error notification

--- src/modules/friends/friends.module.ts
+++ src/modules/friends/friends.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FriendsController } from './friends.controller';
 import { FriendsService } from './friends.service';
 import { PrismaService } from '../../shared/services/prisma.service';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [NotificationsModule],
   controllers: [FriendsController],
   providers: [FriendsService, PrismaService],
   exports: [FriendsService],
