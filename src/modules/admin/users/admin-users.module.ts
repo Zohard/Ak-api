06cb8885452c0c31835a38d76a5b1eac3b95dd51
Zohardus <zohard@hotmail.fr>
2025-09-14 20:42:34 +0200

    Feat: Add Endpoint for user admin

--- src/modules/admin/users/admin-users.module.ts
+++ src/modules/admin/users/admin-users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminUsersController } from './admin-users.controller';
+import { AdminMembersController } from './admin-members.controller';
 import { AdminUsersService } from './admin-users.service';
 import { PrismaService } from '../../../shared/services/prisma.service';
 
 @Module({
-  controllers: [AdminUsersController],
+  controllers: [AdminUsersController, AdminMembersController],
   providers: [AdminUsersService, PrismaService],
   exports: [AdminUsersService],
 })
