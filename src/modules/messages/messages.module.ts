5b874cb9e7203f4c78fe5b691819874377258e3f
Zohardus <zohard@hotmail.fr>
2025-10-10 16:54:25 +0200

    Fix crash vercel deploy

--- src/modules/messages/messages.module.ts
+++ src/modules/messages/messages.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
 import { PrismaService } from '../../shared/services/prisma.service';
+import { EmailService } from '../../shared/services/email.service';
 
 @Module({
   controllers: [MessagesController],
-  providers: [MessagesService, PrismaService],
+  providers: [MessagesService, PrismaService, EmailService],
   exports: [MessagesService],
 })
 export class MessagesModule {}
