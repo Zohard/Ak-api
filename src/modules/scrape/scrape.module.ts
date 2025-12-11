15e41df923af4efc445e977167b7e95ae08cc759
Zohardus <zohard@hotmail.fr>
2025-12-11 21:47:58 +0100

    Fix ScrapeModule dependency injection - add PrismaService
    ScrapeService needs PrismaService but it wasn't available in the module context

--- src/modules/scrape/scrape.module.ts
+++ src/modules/scrape/scrape.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ScrapeController } from './scrape.controller';
 import { ScrapeService } from './scrape.service';
+import { PrismaService } from '../../shared/services/prisma.service';
 
 @Module({
   controllers: [ScrapeController],
-  providers: [ScrapeService],
+  providers: [ScrapeService, PrismaService],
   exports: [ScrapeService],
 })
 export class ScrapeModule {}
